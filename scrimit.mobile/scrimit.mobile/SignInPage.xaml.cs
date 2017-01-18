7dbdd79c3c89bce674acff7f249b0ed6984404d8
dexter <dexter@WIN-DEXTER-PC>
2017-01-18 17:13:50 -0500

    updates

---
+++ scrimit.mobile/scrimit.mobile/SignInPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace scrimit.mobile
+{
+    public partial class SignInPage : ContentPage
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+
+        private void facebookButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void headerImage_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void headerImage_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
